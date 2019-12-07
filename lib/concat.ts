37206ecff48d18744f2e8221c221efaa3ed614dd
Aaron Reisman <aaron@lifeiscontent.net>
2019-12-06 16:24:25 -0800

    Update concat.ts
    Remove type ambiguity

--- lib/concat.ts
+++ lib/concat.ts
@@ -4,6 +4,9 @@
  * @param b second element
  * @returns concatenated array or string
  */
+
+export default function concat<T>(a: T[], b: T[]): T[]
+export default function concat(a: string, b: string): string;
 export default function concat<T>(a: T[] | string, b: T[] | string): T[] | string {
     if (typeof a === 'string' && typeof b === 'string') {
         return a + b;
